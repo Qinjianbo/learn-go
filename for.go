af0b6bcfb5be4ff016eccd75ec168803062183fc
Qinjianbo <279250819@qq.com>
2020-07-01 20:21:34 +0800

    feat: strings 高效字符串处理

--- for.go
+++ for.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	// 每次都要拼接新字符串sep，然后赋值给s
+	// 如果量大的话，代价会很大
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+
+	// 使用strings包的join方法会更高效
+	fmt.Println(strings.Join(os.Args[1:], " "))
+
+	fmt.Println(os.Args[1:])
 }
